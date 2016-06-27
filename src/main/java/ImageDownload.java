1b3995099620c8bb1134d296e77ec616a29a34b7
denispastushenko <dpastushenko1996@gmail.com>
2016-06-27 13:54:37 +0300

    Update ImageDownload.java

--- src/main/java/ImageDownload.java
+++ src/main/java/ImageDownload.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.net.URL;
 
 public class ImageDownload {
-    public static final String FILE_WAY = "Z:/MyWorkSpase/ValentynTests/InstagramTest(4)/VIKUS/Test_image.jpg";
+    public static final String FILE_WAY = "Z:/MyWorkSpase/ValentynTests/InstagramTest(4)/VIKUS/";
 
     public File saver( String url1,String destination) throws IOException {
         URL url = new URL(url1);
